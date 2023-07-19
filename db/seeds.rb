40b5d0130e2c0f68c9851844cb4a66db389fba9f
Trevor2002-maker <trevor.madara@student.moringaschool.com>
2023-07-19 15:38:27 +0300

    Changed password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 # Find or create admin 1
 admin1 = Admin.find_by(username: 'trevor')
-unless admin1
+if admin1
+  admin1.password = 'trev123'  # Replace 'new_password' with the desired new password
+  admin1.save!
+  puts "Admin 1 password changed. New password: #{admin1.password}"
+else
   admin1 = Admin.create!(username: 'trevor', password: 'trev123')
   puts "Admin 1 created with username: #{admin1.username} and password: #{admin1.password}"
-else
-  puts "Admin 1 already exists with username: #{admin1.username}"
 end
 
 # Find or create admin 2
 admin2 = Admin.find_by(username: 'romen')
-unless admin2
+if admin2
+  admin2.password = 'romen123@'  # Replace 'new_password' with the desired new password
+  admin2.save!
+  puts "Admin 2 password changed. New password: #{admin2.password}"
+else
   admin2 = Admin.create!(username: 'romen', password: 'romen123@')
   puts "Admin 2 created with username: #{admin2.username} and password: #{admin2.password}"
-else
-  puts "Admin 2 already exists with username: #{admin2.username}"
 end
