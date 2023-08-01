fa7a2bd3e8dfe2c9b3cd3aec81dd0b27749ce70b
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2023-08-01 15:59:45 +0300

    Render config

--- test/controllers/admin_attendees_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AdminAttendeesControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get admin_attendees_show_url
-    assert_response :success
-  end
-end
