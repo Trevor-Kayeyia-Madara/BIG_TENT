baac403a9dd29de155c0190a88f176ed8524d1ab
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2023-08-01 16:34:07 +0300

    Rails

--- bin/tmk-parking-rails-build.sh
+++ bin/tmk-parking-rails-build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install gems and their dependencies
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+
+# Precompile assets for production
+bundle exec rails assets:precompile
+
+# Clean up assets
+bundle exec rails assets:clean
+
+# Run database migrations
+bundle exec rails db:migrate
